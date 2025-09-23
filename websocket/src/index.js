daee9e637bcdd14b0bcb321b289532104f755940
Awilkes21 <awilkes2020@gmail.com>
2025-09-23 17:44:56 -0400

    Docker setup

---
+++ websocket/src/index.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+
+const PORT = process.env.WS_PORT || 5000;
+const wss = new WebSocket.Server({ port: PORT });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    ws.send(`Echo: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log(`WebSocket server running on port ${PORT}`);
